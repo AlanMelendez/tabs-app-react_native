d65a07f806eb3a7bc12e58fe55e4940951a96ac2
AlanMelendez <alancuevasmelendezalan123@gmail.com>
2025-03-03 02:27:07 -0600

    feat: enhance drawer navigation with custom icons and styling options

--- app/drawer/_layout.tsx
+++ app/drawer/_layout.tsx
@@ -1,17 +1,37 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Drawer } from 'expo-router/drawer';
+import { FontAwesome } from '@expo/vector-icons';
 
 const DrawerLayout = () => {
   return (
-    <Drawer>
-        <Drawer.Screen 
+    <Drawer
+
+     screenOptions={{
+      overlayColor: 'rgba(0, 0, 0, 0.7)',
+      drawerActiveTintColor: 'red',
+      headerShadowVisible: false,
+      sceneContainerStyle: {
+        backgroundColor: 'white',
+      }
+     }}
+    >
+        <Drawer.Screen
           name="user/index"
-          options={{ title: 'Usuario', drawerLabel: 'User' }}
+          options={{ 
+            title: 'Usuario', 
+            drawerLabel: 'User',
+            // drawerIcon: () => <Text>👤</Text>
+            drawerIcon: ({color,size}) => (<FontAwesome size={28} name="user" color={color}/>)
+           }}
         />
         <Drawer.Screen 
           name="schedule/index"
-          options={{ title: 'Schedule', drawerLabel: 'Schedulesxd' }}
+          options={{ 
+            title: 'Schedule', 
+            drawerLabel: 'Schedulesxd',
+            drawerIcon: () => (<FontAwesome size={28} name="calendar" color={'indigo'}/>)
+          }}
         />
     </Drawer>
   )
