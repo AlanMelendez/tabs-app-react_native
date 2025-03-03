14288327dc4e34b5e77f06f4427bda0eb7cab9ac
AlanMelendez <alancuevasmelendezalan123@gmail.com>
2025-03-03 00:12:07 -0600

    add: create a tabs folder; within in tabs folder added another components to show more information

---
+++ app/tabs/_layout.tsx
@@ -0,0 +1,28 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Tabs } from 'expo-router';
+
+const TabsLayout = () => {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'green' }}>
+      <Tabs.Screen
+        name="home/index"
+        options={{
+          
+          title: 'Home',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="favorites/index"
+        options={{
+          title: 'Favorites',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="heart" color={color} />,
+        }}
+      />
+    </Tabs>
+  )
+}
+
+export default TabsLayout
